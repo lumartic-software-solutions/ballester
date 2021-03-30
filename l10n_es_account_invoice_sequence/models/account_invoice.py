7fdfef3c8f1dddb73a1b8c683a71587198c4015e
divya vyas <divyapanchal7594@gmail.com>
2021-03-30 17:37:58 +0530

    solved sequence issue

--- l10n_es_account_invoice_sequence/models/account_invoice.py
+++ l10n_es_account_invoice_sequence/models/account_invoice.py
@@ -10,9 +10,9 @@ from odoo import models, fields, api
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-#     number = fields.Char(related=False, size=32, copy=False)
-#     invoice_number = fields.Char(copy=False)
-    invoice_number = fields.Char(related='move_id.name', store=True, readonly=True, copy=False)
+    number = fields.Char(related=False, size=32, copy=False)
+    invoice_number = fields.Char(copy=False)
+    # invoice_number = fields.Char(related='move_id.name', store=True, readonly=True, copy=False)
 
     def _get_seq_number_next_stuff(self):
         self.ensure_one()
@@ -27,46 +27,46 @@ class AccountInvoice(models.Model):
                 journal_sequence = self.journal_id.invoice_sequence_id
         return journal_sequence, domain
 
-#     @api.multi
-#     def action_move_create(self):
-#         res = super(AccountInvoice, self).action_move_create()
-#         print("\n\n\n  action_move_create=====================")
-#         for inv in self:
-#             if not inv.invoice_number:
-#                 sequence = inv.journal_id.invoice_sequence_id
-#                 print("\n\n\n\n ***************sequence********", sequence)
-#                 if inv.type in {'out_refund', 'in_refund'} and \
-#                         inv.journal_id.refund_inv_sequence_id:
-#                     sequence = inv.journal_id.refund_inv_sequence_id
-#                     print("\n\n\n\n**************id*sequence********", sequence)
-#  
-#                 if sequence:
-#                     sequence = sequence.with_context(
-#                         ir_sequence_date=inv.date or inv.date_invoice,
-#                         ir_sequence_date_range=inv.date or inv.date_invoice,
-#                     )
-#                     number = sequence.next_by_id()
-#                     print("\n\n\n\n****number**", number)
-#                 else:  # pragma: no cover
-#                     # Other localizations or not configured journals
-#                     number = inv.move_id.name
-#                     print("\n\n\n\n****numbe elser**", number)
-#                 print("\n\n\n\nUUUUUUUUU!!!UUUUUUUUUU", number)
-#                 inv.write({
-#                     'number': number,
-#                     'invoice_number': number,
-#                 })
-#  
-#             else:  # pragma: no cover
-#                 print ("\n\n\n\n YYYYYYYYYYYYYYYYY", inv.invoice_number)
-#                 inv.number = inv.invoice_number
-#         for inv in self:
-#             # Include the invoice reference on the created journal item
-#             # This is done for displaying the number on the conciliation
-#             inv.move_id.ref = (
-#                 "{0} - {1}" if inv.move_id.ref else "{1}"
-#             ).format(inv.move_id.ref, inv.invoice_number)
-#         return res
+    @api.multi
+    def action_move_create(self):
+        res = super(AccountInvoice, self).action_move_create()
+        print("\n\n\n  action_move_create=====================")
+        for inv in self:
+            if not inv.invoice_number:
+                sequence = inv.journal_id.invoice_sequence_id
+                print("\n\n\n\n ***************sequence********", sequence)
+                if inv.type in {'out_refund', 'in_refund'} and \
+                        inv.journal_id.refund_inv_sequence_id:
+                    sequence = inv.journal_id.refund_inv_sequence_id
+                    print("\n\n\n\n**************id*sequence********", sequence)
+
+                # if sequence:
+                #     sequence = sequence.with_context(
+                #         ir_sequence_date=inv.date or inv.date_invoice,
+                #         ir_sequence_date_range=inv.date or inv.date_invoice,
+                #     )
+                #     number = sequence.next_by_id()
+                #     print("\n\n\n\n****number**", number)
+                # else:  # pragma: no cover
+                #     # Other localizations or not configured journals
+                number = inv.move_id.name
+                print("\n\n\n\n****numbe elser**", number)
+                print("\n\n\n\nUUUUUUUUU!!!UUUUUUUUUU", number)
+                inv.write({
+                    'number': number,
+                    'invoice_number': number,
+                })
+
+            else:  # pragma: no cover
+                print ("\n\n\n\n YYYYYYYYYYYYYYYYY", inv.invoice_number)
+                inv.number = inv.invoice_number
+        for inv in self:
+            # Include the invoice reference on the created journal item
+            # This is done for displaying the number on the conciliation
+            inv.move_id.ref = (
+                "{0} - {1}" if inv.move_id.ref else "{1}"
+            ).format(inv.move_id.ref, inv.invoice_number)
+        return res
 
     @api.multi
     def unlink(self):
